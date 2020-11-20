da74e9628ba3318c04e67edae9c7a74c420b2958
henrique <henriquelourente@gmail.com>
2020-11-20 18:55:14 -0300

    adding Swagger

--- src/SampleLibrary.Api/Startup.cs
+++ src/SampleLibrary.Api/Startup.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using SampleLibrary.IoC;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace SampleLibrary.Api
 {
@@ -21,6 +23,15 @@ namespace SampleLibrary.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Sample Library ",
+                    Version = "v1",
+                });
+            });
+
             services.AddControllers();
             services.AddDbContext(Configuration);
             services.Register();
@@ -33,6 +44,13 @@ namespace SampleLibrary.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SampleLibrary");
+                c.DocExpansion(DocExpansion.None);
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
